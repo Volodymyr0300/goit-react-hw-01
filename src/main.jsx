68a42bfc864965bf8bbd6b8705a00737dae1c5c5
Volodymyr Poliakov <vladimir03002@gmail.com>
2024-03-23 20:36:47 -0500

    finished first task 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import "modern-normalize";
 import "./index.css";
-import App from "./components/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
